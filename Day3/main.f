5629580b2562940583bce6b18a6ade2b356dfd67
Charles Farris <charlesfarris71@gmail.com>
2020-12-03 12:22:00 -0700

    Implement Part 2

--- Day3/main.f
+++ Day3/main.f
@@ -1,6 +1,7 @@
       program Day3
          character line*1024,token
-         integer map(10000,1024),rows,columns(10000),trees,column,row
+         integer map(10000,1024),rows,columns(10000),column,row
+         integer dr(5),dc(5),sum(5)
 
 c        read input file         
          open(1,FILE='input.txt',STATUS='OLD')
@@ -29,21 +30,40 @@ c        read input file
          enddo
  200     close(1)
 
-         write(*,*) 'Rows: ', rows
+         write(*,*) 'Rows: ',rows
 
-c        iterate down slope
-         column = 1
-         do row=1,rows
-c           check tree            
-            trees = trees+map(row,column)
-            write(*,*) row,column,map(row,column),columns(i)
-c           increment             
-            column = column+3
-            if (column.gt.columns(i)) then
-c              wrap map
-               column = column-columns(i)
-            endif
- 300     enddo
+c        set delta rows         
+         dr(1) = 1
+         dr(2) = 1
+         dr(3) = 1
+         dr(4) = 1
+         dr(5) = 2
+
+c        set delta columns         
+         dc(1) = 1
+         dc(2) = 3
+         dc(3) = 5
+         dc(4) = 7
+         dc(5) = 1
+
+         do 400 i=1,5
+c           iterate down slope
+            sum(i) = 0
+            column = 1
+            do 300 row=1,rows,dr(i)
+c              check tree            
+               sum(i) = sum(i)+map(row,column)
+c              increment             
+               column = column+dc(i)
+               if (column.gt.columns(i)) then
+c                 wrap map
+                  column = column-columns(i)
+               endif
+ 300       continue
+ 400     continue
 c        write number of trees
-         write(*,*) 'Trees: ', trees
+         do 500 i=1,5
+            write(*,*) 'Trees: ',sum(i)
+ 500     continue
+         write(*,*) 'Product: ',sum(1)*sum(2)*sum(3)*sum(4)*sum(5)
       end
