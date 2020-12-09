b1f8953207af0083ec41cea917b4d10c698178a6
Charles Farris <charlesfarris71@gmail.com>
2020-12-08 23:52:39 -0700

    Implement Part 2

--- Day9/main.f
+++ Day9/main.f
@@ -4,7 +4,7 @@
 c           locals            
             double precision values(1024)
             double precision valid,sum,value
-            double precision validate
+            double precision validate,weakness
 
             integer i,j,io,count,preamble
 
@@ -26,17 +26,26 @@ c           read input file
             enddo
             close(1)
 
-c           iterate over the values
+c           preamble for `input.txt`            
             preamble = 25
+
+c           preamble for `test.txt`
+c           preamble = 5            
+
+c           iterate over the values
             do i=preamble+1,count
 
 c               check invalid
                 valid = validate(values,values(i),i-preamble, i-1)
                 if (valid.eq.0) then
+                    write(*,*) 'Part 1'
                     write(*,*) 'Invalid: ',values(i)
-                endif
 
-            enddo
+                    write(*,*) 'Part 2'
+                    sum = weakness(values,count,values(i))
+                    write(*,*) 'Weakness: ',sum
+                endif
+            enddo                
         end
 
 c       validates the value by finding a matching sum in the 
@@ -60,5 +69,52 @@ c                    write(*,*) i,j,values(i),values(j),sum
             enddo
  200        continue
 c           write(*,*) ''
+            return
+        end
+
+c       finds the encryption weakness by returning the sum
+c       of the minimum and maximum values in the preamble values
+        double precision function weakness(values,count,value)
+            implicit none
+
+            double precision values(*),value,sum
+            double precision minimum,maximum
+            integer count,i,j
+
+c           iterate over values
+            weakness = -1
+            do i=1,count-1
+c               initial minimum, maximum, and sum
+                minimum = values(i)
+                maximum = values(j)
+                sum = values(i)
+
+c               build continguous range
+                do j=i+1,count
+
+c                   update minimum
+                    if(values(j).lt.minimum) then
+                        minimum = values(j)
+                    endif
+
+c                   update maximum
+                    if(values(j).gt.maximum) then
+                        maximum = values(j)
+                    endif
+
+c                   update sum                    
+                    sum = sum+values(j)
+
+c                   check sum                    
+                    if(sum.eq.value) then                        
+                        weakness = minimum + maximum
+                        goto 300
+                    else if(sum.gt.value) then
+                        goto 325
+                    endif
+                enddo
+ 325            continue
+            enddo
+ 300        continue            
             return
         end
