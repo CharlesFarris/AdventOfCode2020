b7d36d0e885dc34ffa2d2c882605f87bc9b712eb
Charles Farris <charlesfarris71@gmail.com>
2020-12-12 09:00:40 -0700

    Implement Part 2

--- Day12/main.f
+++ Day12/main.f
@@ -3,8 +3,9 @@
 
             integer count,io
             character instructions(1024)*1,instruction*1
-            integer values(1024),value,i
+            integer values(1024),value,i,j
             integer x,y,heading
+            integer wx,wy,c,s,tx,ty
 
 c           read instructions            
             count = 0
@@ -23,7 +24,7 @@ c           read instructions
                 endif
             enddo
 
-c           execute instructions            
+c           execute instructions (part 1)
             x = 0
             y = 0
             heading = 90
@@ -64,6 +65,65 @@ c           execute instructions
 
             write(*,*) 'Part 1:'
             write(*,*) 'Manhattan Distance: ',abs(x)+abs(y)
+            write(*,*) ''
+
+
+c           execute instructions (part 2)
+            x = 0
+            y = 0
+            wx = 10
+            wy = 1
+            do i=1,count
+                write(*,*) i,instructions(i),values(i)
+                write(*,*) x,y,wx,wy
+                if (instructions(i).eq.'R') then
+                    if(values(i).eq.90) then
+                        c = 0
+                        s = -1
+                    else if(values(i).eq.180) then
+                        c = -1
+                        s = 0
+                    else if(values(i).eq.270) then
+                        c = 0
+                        s = 1
+                    endif
+                    tx = c*wx - s*wy
+                    ty = s*wx + c*wy
+                    wx = tx
+                    wy = ty
+                else if(instructions(i).eq.'L') then
+                    if(values(i).eq.90) then
+                        c = 0
+                        s = 1
+                    else if(values(i).eq.180) then
+                        c = -1
+                        s = 0
+                    else if(values(i).eq.270) then
+                        c = 0
+                        s = -1
+                    endif
+                    tx = c*wx - s*wy
+                    ty = s*wx + c*wy
+                    wx = tx
+                    wy = ty
+                else if(instructions(i).eq.'N') then
+                    wy = wy+values(i)
+                else if (instructions(i).eq.'E') then
+                    wx = wx+values(i)
+                else if(instructions(i).eq.'S') then
+                    wy = wy-values(i)
+                else if(instructions(i).eq.'W') then
+                    wx = wx-values(i)
+                else if(instructions(i).eq.'F') then
+                    x = x+wx*values(i)
+                    y = y+wy*values(i)
+                endif
+                write(*,*) x,y,wx,wy
+            enddo
+
+            write(*,*) 'Part 2:'
+            write(*,*) 'Manhattan Distance: ',abs(x)+abs(y)
+            write(*,*) ''
 
         end
 
